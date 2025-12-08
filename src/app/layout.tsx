95993d342118611d9809ea9f90dadb0ed3aec039
Squalood <adriancas98@gmail.com>
2025-12-08 00:13:17 -0700

    cambio de iconos y tipografia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const urbanist = Urbanist({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Clinica de Neurología>",
   description: "Clinica de Neurología de Dr. Jose Orlando Guinto Nava",
+  icons: {
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", type: "image/x-icon" },  
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180" }, 
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={urbanist.className}
       >
         {children}
         <Footer />
